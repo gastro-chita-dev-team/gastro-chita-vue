ddff551a904b5b8f9016bf3dc044a9b2b9231062
Dolet B <doletbizhev@gmail.com>
2020-04-18 14:08:01 +0300

    deploy

---
+++ precache-manifest.5844bf24730ecf81e0f32861564450c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c0a6b0cc4b671b702d",
+    "url": "/gastro-chita-vue/css/app.0f65f587.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/gastro-chita-vue/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "72a06f8f2a8fa17b444095d8aeef8997",
+    "url": "/gastro-chita-vue/index.html"
+  },
+  {
+    "revision": "eb81e3cee0786118eaa7",
+    "url": "/gastro-chita-vue/js/about.c22e2c23.js"
+  },
+  {
+    "revision": "69c0a6b0cc4b671b702d",
+    "url": "/gastro-chita-vue/js/app.0457bbe8.js"
+  },
+  {
+    "revision": "491201c027b03ea99a2a",
+    "url": "/gastro-chita-vue/js/chunk-vendors.d296baf7.js"
+  },
+  {
+    "revision": "cab57e304630fb1e3ac37a3a258d7505",
+    "url": "/gastro-chita-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gastro-chita-vue/robots.txt"
+  }
+]);
